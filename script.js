5902df2eafbe0ba7baa0d89a7b58f74a1b7ef299
lastlyhi <lastlyhiyt@gmail.com>
2024-11-01 23:21:25 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Change the heading text when the button is clicked
+document.getElementById("change-text-btn").addEventListener("click", function() {
+    document.getElementById("welcome-heading").textContent = "hooray you pressed a button";
+});
