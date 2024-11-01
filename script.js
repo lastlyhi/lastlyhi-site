35b96f3dd0cb6d4f6556ef5c7476e6d734e28fb1
lastlyhi <lastlyhiyt@gmail.com>
2024-11-01 23:20:59 +0800

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-// Change the heading text when the button is clicked
-document.getElementById("change-text-btn").addEventListener("click", function() {
-    document.getElementById("welcome-heading").textContent = "hooray you pressed a button";
-});
