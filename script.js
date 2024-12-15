5b87c218385485aa28c7ec0faa7dc6027f2a25d2
lastlyhi <lastlyhiyt@gmail.com>
2024-12-15 14:52:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 // Change the heading text when the button is clicked
 document.getElementById("change-text-btn").addEventListener("click", function() {
-    document.getElementById("welcome-heading").textContent = "hooray you pressed a button";
+    document.getElementById("welcome-heading").textContent = "call the number yourself, too broke to hire website designer";
 });
